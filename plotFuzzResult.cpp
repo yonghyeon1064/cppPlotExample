48229e4166eadf1815346e5f083568f0fd346489
Kim YongHyeon <yonghyeon1064@gmail.com>
2022-05-02 03:07:43 +0000

    try to draw multiple graph in plane. but fail

--- plotFuzzResult.cpp
+++ plotFuzzResult.cpp
@@ -2,16 +2,29 @@
 #include "supportLib.hpp"
 
 int main(){
-    RGBABitmapImageReference *imageReference = CreateRGBABitmapImageReference();
+    RGBABitmapImageReference *imageReference1 = CreateRGBABitmapImageReference();
+    RGBABitmapImageReference *imageReference2 = CreateRGBABitmapImageReference();
+    RGBABitmapImage *combined = CreateImage(2000, 2000, GetWhite());
+    RGBABitmapImage *image1, *image2;
 
     std::vector<double> x = {-2, -1, 0, 1, 2};
-    std::vector<double> y = {1, 2, 3, 4, 5};
+    std::vector<double> y1 = {1, 2, 3, 4, 5};
+    std::vector<double> y2 = {5, 4, 3, 2, 1};
 
-    DrawScatterPlot(imageReference, 1000, 1000, &x, &y);
+    DrawScatterPlot(imageReference1, 2000, 2000, &x, &y1);
+    DrawScatterPlot(imageReference1, 2000, 2000, &x, &y2);
 
-    std::vector<double> *pngData = ConvertToPNG(imageReference->image);
+    image1 = imageReference1->image;
+    image2 = imageReference2->image;
+
+    DrawImageOnImage(combined, image1, 0, 0);
+    //DrawImageOnImage(combined, image2, 0, 0);
+
+    std::vector<double> *pngData = ConvertToPNG(combined);
     WriteToFile(pngData, "fuzzPlot.png");
-    DeleteImage(imageReference->image);
+    DeleteImage(imageReference1->image);
+    DeleteImage(imageReference2->image);
+    DeleteImage(combined);
 
 
     return 0;
